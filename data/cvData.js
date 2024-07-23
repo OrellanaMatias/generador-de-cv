f3f25cdc70dfe7f6e6fc2f05ac7a3fb0d7cdee01
Orellana Matias <meorellanaramirez@gmail.com>
2024-07-22 22:36:15 -0300

    Si

---
+++ data/cvData.js
@@ -0,0 +1,136 @@
+export const cvData = {
+  name: "Matias Orellana",
+  image: "",
+  jobTitle: "Desarrollador",
+  location: "Buenos Aires, Argentina",
+  email: "tucorreo@gmail.com",
+  linkedIn: "",
+  instagram: "",
+  facebook: "",
+  twitter: "",
+  github: "OrellanaMatias",
+  website: "orellanamatias.com.ar",
+  about:
+    "About me",
+
+  toolsAndTechSkills: [
+    "Photoshop",
+    "Illustrator",
+    "Figma",
+    "Git",
+    "GitHub",
+    "Illustrator",
+  ],
+  industryKnowledge: [
+    "Javascript",
+    "CSS",
+    "HTML",
+    "Next.js",
+    "React",
+    "Tailwind",
+    "PHP",
+    "Node.js",
+    "Three.js",
+    "MySQL",
+    "Bootstrap"
+  ],
+  languages: ["Espñaol(Nativo)", "Ingles(Básico)"],
+  skillTitle1: "Herramientas y habilidades",
+  skillTitle2: "Tegnologias",
+  skillTitle3: "Idiomas",
+  projects: [
+    {
+      title: "Proyecto 1",
+      link: "https://orellanamatias.com.ar",
+      summary: `Descripcion de en que consiste este proyecto`,
+    },
+    {
+      title: "Proyecto 2",
+      link: "https://orellanamatias.com.ar",
+      summary: `Descripcion de en que consiste este proyecto`,
+    },
+
+    {
+      title: "Proyecto 3",
+      link: "https://orellanamatias.com.ar",
+      summary: `Descripcion de en que consiste este proyecto`,
+    },
+  ],
+  education: [
+    {
+      title: "Tecnico en informatica personal y profesional",
+      school: "Secundaria cualquiera",
+      startDate: "2022",
+      endDate: "Ahora",
+    },
+    {
+      title: "Programador experto aja",
+      school: "Universidad",
+      startDate: "2018",
+      endDate: "2019",
+    },
+  ],
+  experiences: [
+    {
+      title: "Experiencia laboral 1",
+      company: "Empresa 1",
+      startDate: "2024",
+      endDate: "2024",
+      current: true,
+      summary: `Descripcion de esta experiencia laboral`,
+    },
+    {
+      title: "Experiencia laboral 2",
+      company: "Empresa 2",
+      startDate: "2024",
+      endDate: "2024",
+      current: true,
+      summary: `Descripcion de esta experiencia laboral`,
+    },
+  ],
+  displayImage: true,
+  displayMail: true,
+  displayWebSite: true,
+  displayLinkedIn: false,
+  displayInstagram: false,
+  displayFacebook: false,
+  displayGithub: true,
+  displayTwitter: true,
+  activeColor: "#5B21B6",
+};
+
+/*
+export const cvDataEmpty = {
+  name: "",
+  jobTitle: "",
+  location: "",
+  email: "",
+  linkedin: "",
+  twitter: "",
+  github: "",
+  website: "",
+  aboutme: "",
+  toolsAndTechSkills: [],
+  industryKnowledge: [],
+  languages: [],
+  projects: [
+    {
+      title: "",
+      summary: "",
+    },
+  ],
+  experience: [
+    {
+      title: "",
+      company: "",
+      from: new Date(),
+      to: new Date(),
+      current: true,
+      summary: "",
+    },
+  ],
+  displayEducation: false,
+  displayProjects: false,
+  activeColor: "#5B21B6",
+};
+*/
