e107fba924488fdf65dd0d95606dd3af1b0f3681
Orellana Matias <meorellanaramirez@gmail.com>
2024-07-22 22:35:34 -0300

    Commit inicial

---
+++ constants/message-result.constants.js
@@ -0,0 +1,3 @@
+export const FILE_READ_ERROR = "Hubo un error al intentar leer el archivo :(";
+export const UNSUPPORTED_FILE_TYPE = "Tipo de archivo no soportado";
+export const FILE_NOT_SELECTED = "No seleccionaste ningun archivo crack";
